6b3edfe58ccc5ddf3cd7d717dbf05f25431d4537
TimaSv-Uk <sviridenkotimofei@gmail.com>
2025-06-27 14:28:05 +0000

    add readme

--- task1/task1_2.py
+++ task1/task1_2.py
@@ -9,10 +9,14 @@ def generate_matrix(mod=2, num_of_x=3):
 
 def main():
     adjasonsy_matrix = []
-    # print(generate_matrix())
-    matrix_X = generate_matrix(2, 3)
-    matrix_Y = generate_matrix(2, 3)
-
+    mod = 2
+    num_of_x = 3
+    matrix = generate_matrix(mod, num_of_x)
+    matrix_X = matrix
+    matrix_Y = matrix
+    element_len = len(matrix_X)
+    print(matrix_X)
+    print(len(matrix_X))
     for x in matrix_X:
         for y in matrix_Y:
             if ((x[1] + y[1]) % 2 == (x[0] * y[0]) % 2) and (
@@ -24,20 +28,22 @@ def main():
                 adjasonsy_matrix.append([x, y, 0])
     # x2 + y2 = x1 * y1
     # x3 + y3 = x1 * y2
-    solution_matrix = list(batched([element[2] for element in adjasonsy_matrix], 8))
-    print(solution_matrix)
+    solution_matrix = list(
+        batched([element[2] for element in adjasonsy_matrix], element_len)
+    )
+    # print(solution_matrix)
 
     f = open("solution_task1_2.txt", "w")
 
     f.write("   ")
     for i, _ in enumerate(solution_matrix):
-        f.write(f"y{i + 1} ")
+        f.write(f"y{i + 1}\t")
 
     f.write("\n")
     for i, col in enumerate(solution_matrix):
         f.write(f"x{i + 1} ")
         for element in col:
-            f.write(str(element) + "  ")
+            f.write(str(element) + "\t")
 
         f.write("\n")
 
